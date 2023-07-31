4a45413fdecef568859b9e523e36a504db173650
Shaheer Sheeraz <shaheer@shaheers-air.home>
2023-07-31 17:39:45 -0400

    connecting webapp with Mongoose

--- todolist-WebApp/app.js
+++ todolist-WebApp/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -8,10 +9,33 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+// creating a new database
+mongoose.connect("mongodb://localhost:27017/listDB");
 
-app.get("/", function(req, res) {
+// mongoose schema, with field name and type
+const itemsSchema = {
+  name: String
+}
+
+// new mongoose model based on the schema
+const Item = mongoose.model("Item", itemsSchema);
+
+// creating and adding default items to the database
+const default1 = new Item ({
+  name: "Welcome to the To-do List!"
+});
+
+const default2 = new Item ({
+  name: "Click + to add a new item"
+});
+
+const default3 = new Item ({
+  name: "Click the check to delete an item"
+});
+
+const defaultItems = [default1, default2, default3];
+
+app.get("/", async (req, res) =>  {
 
   const currentDate = new Date();
   const options = {
@@ -23,20 +47,66 @@ app.get("/", function(req, res) {
 
   const formattedDate = currentDate.toLocaleDateString(undefined, options);
 
-  res.render("list", { date: formattedDate, newListItems: items });
+  // error handling
+  const db = mongoose.connection;
 
+  db.on("error", (error) => {
+    console.error("Database connection error:", error);
+  });
+  
+  // tap into model and find everything in the items collection
+  // will display the default items ensuring they don't stack
+  // render the items that are present in the database
+  try {
+    const foundItems = await Item.find({});
+    if (foundItems.length === 0) {
+      db.once("open", async () => {
+        console.log("Connected to the database");
+        try {
+          await Item.insertMany(defaultItems);
+          console.log("Default items inserted successfully:");
+        } catch (err) {
+          console.error(err);
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { date: formattedDate, newListItems: foundItems });
+    }
+  } catch (err) {
+    console.error("Error fetching items:", err);
+    res.status(500).send("Error fetching items.");
+  }
 });
 
-app.post("/", function(req, res){
+app.post("/", async (req, res) => {
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
+  const item = new Item({
+    name: itemName
+  });
+
+  // after we save our item, we redirect so that we can FIND all items and render them on screen
+  try {
+    await item.save();
     res.redirect("/");
+  } catch (err) {
+    console.error("Error saving item:", err);
+    res.status(500).send("Error saving item.");
+  }
+});
+
+app.post("/delete", async (req, res) => {
+
+  const itemToDelete = req.body.deleteItem;
+
+  try {
+    await Item.findByIdAndRemove(itemToDelete);
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error in deleting item:", err);
+    res.status(500).send("Error deleting item.");
   }
 });
 
