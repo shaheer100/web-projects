091372b2248ce882be088643917cc1cec3037274
Shaheer Sheeraz <shaheer@shaheers-air.home>
2023-08-08 16:13:25 -0400

    cookies and authorization added

--- Blog-WebApp/app.js
+++ Blog-WebApp/app.js
@@ -8,6 +8,8 @@ import session from "express-session";
 import passport from "passport";
 import passportLocalMongoose from "passport-local-mongoose";
 
+const auth = false;
+
 // getting the formatted date (Month day, year)
 const currentDate = new Date();
 const monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -40,25 +42,20 @@ mongoose.connect("mongodb+srv://shaheersheeraz22:Test123@cluster0.ndvvhaa.mongod
 
 // mongoose schemas, with field name and type
 const postsSchema = new mongoose.Schema ({
-/*   user: {
+  user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-  }, */
+  },
   title: String,
   body: String,
-  date: String
+  date: String,
 });
 
 const usersSchema = new mongoose.Schema ({
   username: String,
   email: String,
+  aboutMe: String,
   Password: String,
-/*   posts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Post",
-    },
-  ], */
 });
 
 // used to hash + salt passwords, and save users into MongoDB database
@@ -73,40 +70,88 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get("/", async (req, res) => {
-  const address = req.url;
-  try {
-    const posts = await Post.find({});
-    res.render("home.ejs", { address: address, posts: posts});
-  } catch (err) {
-    console.error("Error loading posts:", err);
-    res.status(500).send("Error loading posts.");
+app.get("/register", (req, res) => {
+  if (req.isAuthenticated()) {
+    auth = true;
   }
+  console.log(auth);
+  const address = req.url;
+
+  const userID = auth ? req.user._id : "";
+
+  res.render("register.ejs", { address: address, auth: auth, userID: userID });
+});
+
+app.post("/register", async (req, res) => {  
+  const address = req.url;
+  const { username, aboutMe, email, password } = req.body;
+
+  const newUser = new User({
+    username: username,
+    aboutMe: aboutMe,
+    email: email
+  })
+
+  User.register(newUser, password, (err, user) => {
+    if (err) {
+      console.log(err);
+      res.render("register", { address: address, error: err });
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/");
+      });
+    }
+  });
 });
 
 app.get("/login", (req, res) => {
+  if (req.isAuthenticated()) {
+    auth = true;
+  }
+  console.log(auth);
   const address = req.url;
-  res.render("login.ejs", { address: address });
+
+  const userID = auth ? req.user._id : "";
+
+  res.render("login.ejs", { address: address, auth: auth, userID: userID });
 });
 
-app.get("/register", (req, res) => {
-  const address = req.url;
-
-  User.register({ username: req.body.username }, req.body.password, (err, user) => {
-    if (err) {
-      console.log(err);
-      res.redirect("/register");
-    } else {
-
-    }
+app.post("/login", (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
   });
 
-  res.render("register.ejs", { address: address });
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/");
+      });
+    }
+  });
+});
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
 });
 
 app.get("/compose", (req, res) => {
+  if (req.isAuthenticated()) {
+    auth = true;
+  }
+  console.log(auth);
   const address = req.url;
-  res.render("compose.ejs", { address: address });
+
+  const userID = auth ? req.user._id : "";
+
+  if (req.isAuthenticated()) {
+    res.render("compose.ejs", { address: address, auth: auth, userID: userID });
+  } else {
+    res.render("login.ejs", { address: address, auth: auth, userID: userID });
+  }
 });
 
 app.post("/compose", async (req, res) => {
@@ -114,6 +159,7 @@ app.post("/compose", async (req, res) => {
   const postContent = req.body.postBody;
 
   const post = new Post({
+    user: req.user._id,
     title: postTitle,
     body: postContent,
     date: formattedDate
@@ -129,20 +175,57 @@ app.post("/compose", async (req, res) => {
 });
 
 app.get("/posts/:postID", async (req, res) => {
+  if (req.isAuthenticated()) {
+    auth = true;
+  }
+  console.log(auth);
   const address = req.url;
+
+  const userID = auth ? req.user._id : "";
   const postID = req.params.postID;
   try {
     const requestedPost = await Post.findOne({ _id: postID });
-    res.render("post.ejs", { title: requestedPost.title, body: requestedPost.body, address: address, date: requestedPost.date });
+    res.render("post.ejs", { title: requestedPost.title, body: requestedPost.body, address: address, date: requestedPost.date, auth: auth, userID: userID });
   } catch (err) {
     console.error("Error finding specified post:", err);
     res.status(500).send("Error finding specified post.");
   }
 });
 
-app.get("/users/:userID", async(req, res) => {
+app.get("/users/:userID", async (req, res) => {
+  if (req.isAuthenticated()) {
+    auth = true;
+  }
+  console.log(auth);
   const address = req.url;
-  const userID = req.params.userID;
+
+  const userID = req.user._id;
+
+  try {
+    const requestedUser = await User.findOne({ _id: req.user._id });
+    res.render("user.ejs", { username: requestedUser.username, address: address, auth: auth, userID: userID });
+  } catch (err) {
+    console.error("Error finding specified user:", err);
+    res.status(500).send("Error finding specified user.");
+  }
+});
+
+app.get("/", async (req, res) => {
+  if (req.isAuthenticated()) {
+    auth = true;
+  }
+  console.log(auth);
+  const address = req.url;
+  
+  const userID = auth ? req.user._id : "";
+  
+  try {
+    const posts = await Post.find({});
+    res.render("home.ejs", { address: address, posts: posts, auth: auth, userID: userID });
+  } catch (err) {
+    console.error("Error loading posts:", err);
+    res.status(500).send("Error loading posts.");
+  }
 });
 
 // Start the server
